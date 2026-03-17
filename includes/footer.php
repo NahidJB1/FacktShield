c117ddbd4848aec68b30289ee171b55e9f7f9ca9
NAHID JAHAN BHUIYAN <nahidjahanb1@gmail.com>
2026-03-17 23:55:00 +0800

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<footer class="site-footer">
+    <div class="footer-container">
+        <div class="footer-brand">
+            <div class="footer-logo">
+                <img src="assets/images/logo.svg" alt="FactShield Logo">
+                <span>FACTSHIELD</span>
+            </div>
+            <p class="footer-tagline">Fighting misinformation,<br>one fact at a time.</p>
+        </div>
+
+        <div class="footer-copyright">
+            <p>&copy; 2026 FACTSHIELD Team AstroBytes</p>
+        </div>
+
+        <div class="footer-links">
+            <a href="#" class="blockchain-link">View on Blockchain Explorer &rarr;</a>
+        </div>
+    </div>
+</footer>
+
+<script src="assets/js/global.js"></script>
+</body>
+</html>
